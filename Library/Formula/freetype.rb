5de8e25b2a0b3695ab822f50bd79815475e49cb8
Boris Gordon <boris@boztek.net>
2010-02-02 17:38:27 +1100

    freetype formula updated to 2.3.11

--- Library/Formula/freetype.rb
+++ Library/Formula/freetype.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Freetype <Formula
-  @url='http://mirror.dknss.com/nongnu/freetype/freetype-2.3.9.tar.gz'
+  @url='http://downloads.sourceforge.net/project/freetype/freetype2/2.3.11/freetype-2.3.11.tar.gz'
   @homepage='http://freetype.sourceforge.net'
-  @md5='9c2744f1aa72fe755adda33663aa3fad'
+  @md5='a693c9a4b0121890ca71e39364ffea4a'
 
 # def deps
 #   BinaryDep.new 'cmake'
